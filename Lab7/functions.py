a290103e652a0db71abac703681332a1503fceab
Violetta Sergienko <v.serhiyenko_fit_b_d_6_21@knute.edu.ua>
2021-11-29 00:53:52 +0200

    1

--- Lab7/functions.py
+++ Lab7/functions.py
@@ -1,20 +1,7 @@
 import math
-import pylab
-import numpy as np
-
-
-def function(x):
-    """
-    cos(x)
-    """
-    if x == 0:
-        return 1.0
-    return math.cos(x ** 2) / x
-
-
-x_min = 0
-dx = 0.01
-x_list = np.arange(x_min, dx)
-y_list = [function(x) for x in x_list]
-pylab.plot(y_list)
-pylab.show()
+from numpy import *
+import matplotlib.pyplot as plt
+x = linspace(0, 5, 1000)
+y = -5 * cos(10 * x) * sin(3 * x) / x ** x
+plt.plot(x, y)
+plt.show()
